863ef3979d9d75b0b0cb4e69b0fd1dcb5e818433
fitsumgetachew <fitsumgeta2021@gmail.com>
2024-03-31 23:00:18 +0530

    added add voter , add candidate and start election functions

--- Contracts/election.sol
+++ Contracts/election.sol
@@ -17,4 +17,38 @@ contract Election {
         address delegate;
         uint256 vote;
     }
+// State variables
+    address public admin;
+    bool public electionStarted;
+    mapping(address => Voter) public voters;
+    Candidate[] public candidates;
+
+    // Constructor to set the admin
+    constructor() {
+        admin = msg.sender;
+    }
+
+    // Modifier to restrict access to the admin
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admin can call this function");
+        _;
+    }
+
+    // Function to add a new candidate
+    function addCandidate(string memory _name, string memory _proposal) public onlyAdmin {
+        require(!electionStarted, "Cannot add candidate after election has started");
+        candidates.push(Candidate(_name, _proposal, 0));
+    }
+
+    // Function to add a new voter
+    function addVoter(address _voter) public onlyAdmin {
+        require(!voters[_voter].isRegistered, "Voter already registered");
+        voters[_voter].isRegistered = true;
+    }
+
+    // Function to start the election
+    function startElection() public onlyAdmin {
+        require(!electionStarted, "Election already started");
+        electionStarted = true;
+    }
 }
